27e8bdccd869ed6bf4da5f44a2993f9b2b3e39ee
eswar <eswara@mit.edu>
2018-05-19 20:32:16 -0400

    Adding sts vietnam timeline

---
+++ Vietnam/js/index.js
@@ -0,0 +1,29 @@
+
+$.js = function (el) {
+    return $('[data-js=' + el + ']')
+};
+
+function carousel() {
+  $.js('timeline-carousel').slick({
+    infinite: false,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed:7000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }]
+  });
+
+
+}
+
+carousel();
