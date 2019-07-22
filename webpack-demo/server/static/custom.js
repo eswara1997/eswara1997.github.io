44b2799398515efe6b7c629bfd574469dd79f6b0
eswar <eswara@mit.edu>
2019-07-21 21:34:58 -0400

    testing ml-aframe

---
+++ webpack-demo/server/static/custom.js
@@ -0,0 +1,2 @@
+
+require('ml-aframe');
---
+++ webpack-demo/src/index.js
@@ -0,0 +1,9 @@
+import { groupBy } from "lodash-es"
+import people from "./people"
+require('ml-aframe');
+
+const managerGroups = groupBy(people, "manager")
+
+const root = document.createElement("div")
+root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
+document.body.appendChild(root)
